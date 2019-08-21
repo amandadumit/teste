40a0b7c8df9457759df691f288fe4f7830a6c2b9
Victor Mello <victor.pessoa@usp.br>
2019-08-21 11:21:39 -0300

    created new files

---
+++ code/is_zero.py
@@ -0,0 +1,7 @@
+x = input('tell me something girl: ')
+if(x == '0' or x == 'zero'):
+    print('it is zero, indeed')
+elif(x == 'are you happy in this modern world?'):
+    print('or do you need more')
+else:
+    print('wrong answer')
