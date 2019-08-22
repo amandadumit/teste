e29e4fcc718c7fd52b1005d2212978c3b94e8272
Victor Mello <victor@hotmail.com>
2019-08-22 14:50:20 -0300

    comment on file is_zero.py

--- code/is_zero.py
+++ code/is_zero.py
@@ -1,4 +1,4 @@
-x = input('tell me something girl: ')
+x = input('tell me something girl: ') #comentario
 if(x == '0' or x == 'zero'):
     print('it is zero, indeed')
 elif(x == 'are you happy in this modern world?'):
